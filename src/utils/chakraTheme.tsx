47c40fd47ae03c014c8d6dce4ffcadc8e0713800
Tim <harvard90873@gmail.com>
2021-12-23 11:31:44 +0100

    export config too

--- src/utils/chakraTheme.tsx
+++ src/utils/chakraTheme.tsx
@@ -1,6 +1,6 @@
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
-const config: ThemeConfig = {
+export const config: ThemeConfig = {
   initialColorMode: "dark",
   useSystemColorMode: false,
 };

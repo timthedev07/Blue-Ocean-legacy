1bfa6fc761b1a5b9ec966f9b854e1b6f731af510
Tim <harvard90873@gmail.com>
2021-12-23 11:26:33 +0100

    wrap with chakra provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </Layout>
   );
 };

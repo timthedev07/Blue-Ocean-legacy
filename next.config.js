72cb2daac49ca0c1e4102a49ac3b4054f11eec57
Tim <harvard90873@gmail.com>
2021-12-27 17:34:56 +0100

    prepare for i18n

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  i18n: {
+    /**
+     * Provide the locales you want to support in your application
+     */
+    locales: ["en-US", "es-ES"],
+    /**
+     * This is the default locale you want to be used when visiting
+     * a non-locale prefixed path.
+     */
+    defaultLocale: "en-US",
+  },
+};

61b942c6ab3b9d430abf39ef53952595f50fe8ed
Tim <harvard90873@gmail.com>
2021-12-27 21:26:45 +0100

    include language toggle in nav

--- src/components/navigation/Nav.tsx
+++ src/components/navigation/Nav.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { FC } from "react";
+import { LanguageToggle } from "../LanguageToggle";
 
 interface NavProps {
   className?: string;
@@ -21,7 +22,7 @@ export const Nav: FC<NavProps> = ({ className }) => {
     <nav
       className={
         className +
-        " w-full h-12 bg-slate-900 flex pl-10 items-center z-50 sticky top-0"
+        " w-full h-12 bg-slate-900 flex pl-10 items-center z-50 sticky top-0 px-4"
       }
     >
       <Link href="/" passHref>
@@ -31,7 +32,7 @@ export const Nav: FC<NavProps> = ({ className }) => {
           className="h-[65%] w-auto cursor-pointer"
         />
       </Link>
-      <ul className="w-full max-w-6xl h-full flex justify-around items-center">
+      <ul className="w-full max-w-6xl h-full flex justify-around items-center relative">
         {NAV_LINKS.map(({ href, name }) => (
           <Link key={name} href={href} passHref>
             <li className="cursor-pointer text-neutral-400 transition duration-200 hover:text-slate-50">
@@ -40,6 +41,7 @@ export const Nav: FC<NavProps> = ({ className }) => {
           </Link>
         ))}
       </ul>
+      <LanguageToggle className="" />
     </nav>
   );
 };

17ea2cde72cdcc307bb84543813aabe7307f846e
Tim <harvard90873@gmail.com>
2021-12-30 20:34:11 +0100

    add some translations for home page

---
+++ src/translations/index.ts
@@ -0,0 +1,16 @@
+export type IndexTranslationType = typeof enIndex;
+
+export const enIndex = {
+  hero: "Blue Ocean International Trade",
+  heroSubheading: "An International Trading Business",
+};
+
+export const esIndex: IndexTranslationType = {
+  hero: "Blue Ocean Comercio Internacional",
+  heroSubheading: "Un Negocio de Comercio Internacional",
+};
+
+export const zhIndex: IndexTranslationType = {
+  hero: "蓝海国际",
+  heroSubheading: "国际贸易公司",
+};

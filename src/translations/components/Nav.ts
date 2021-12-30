63bef4a4ae160f6a87b689163778cb9086f2a54a
Tim <harvard90873@gmail.com>
2021-12-30 21:24:39 +0100

    nav translation

---
+++ src/translations/components/Nav.ts
@@ -0,0 +1,19 @@
+export type NavTranslationType = typeof enNav;
+
+export const enNav = {
+  aboutUs: "About Us",
+  products: "Products",
+  contactUs: "Contact Us",
+};
+
+export const esNav: NavTranslationType = {
+  aboutUs: "Nosotros",
+  contactUs: "Contactarnos",
+  products: "Productos",
+};
+
+export const zhNav: NavTranslationType = {
+  aboutUs: "关于我们",
+  contactUs: "联系我们",
+  products: "产品",
+};

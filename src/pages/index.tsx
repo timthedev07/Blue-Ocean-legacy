c4d22725ec1f823bca8d5de00b6ea22f45164d13
Tim <harvard90873@gmail.com>
2021-12-31 16:38:07 +0100

    use translation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -76,7 +76,7 @@ const Home: NextPage = () => {
               },
             }}
           >
-            Our vision
+            {translation.visionHeading}
           </motion.h3>
           {showCover ? (
             <motion.ul

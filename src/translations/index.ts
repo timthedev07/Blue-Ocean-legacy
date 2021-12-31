768f30bd47fcb63afab516bba49f2b5b2bfc6697
Tim <harvard90873@gmail.com>
2021-12-31 12:52:31 +0100

    add translations

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -3,14 +3,29 @@ export type IndexTranslationType = typeof enIndex;
 export const enIndex = {
   hero: "Blue Ocean International Trade",
   heroSubheading: "An International Trading Business",
+  spotlight: [
+    "Wide range of products - From shopping goods to convenient tools, we bring them to you from anywhere in the world.",
+    "We provide you with high-quality products from reliable suppliers.",
+    "We are seeking to build a global reputation and offer our products to more customers.",
+  ],
 };
 
 export const esIndex: IndexTranslationType = {
   hero: "Blue Ocean Comercio Internacional",
   heroSubheading: "Un Negocio de Comercio Internacional",
+  spotlight: [
+    "Amplia gama de productos: desde artículos comerciales hasta herramientas convenientes, se los traemos desde cualquier parte del mundo.",
+    "Le proporcionamos productos de alta calidad de proveedores confiables.",
+    "Intentamos construir una reputación global y ofrecer nuestros productos a más clientes.",
+  ],
 };
 
 export const zhIndex: IndexTranslationType = {
   hero: "蓝海国际",
   heroSubheading: "国际贸易公司",
+  spotlight: [
+    "广泛的销售范围 - 从选购商品到便利工具，我们从世界各地带到你手里",
+    "我们给你提供从可靠的提供商进购高质量产品",
+    "我们正在努力建立全球声誉，并将我们的产品提供给更多客户。",
+  ],
 };

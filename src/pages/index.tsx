f88ffce3d1ba9e64988f74d239f3108ae4e1b0de
Tim <harvard90873@gmail.com>
2021-12-31 12:52:48 +0100

    more content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,10 @@ const fadeIn = {
   },
 };
 
+const spotlightPicSize = "w-40 h-40";
+const spotlightItem =
+  "flex flex-col justify-around items-center text-center p-7 gap-5 grow shrink basis-0";
+
 const Home: NextPage = () => {
   const router = useRouter();
   const locale = router.locale as LocaleType;
@@ -28,22 +32,49 @@ const Home: NextPage = () => {
   return (
     <motion.div exit={{ opacity: 0 }} initial="initial" animate="animate">
       {/* Hero */}
-      <div className="w-full flex flex-col items-center justify-center h-[550px] bg-cyan-600 text-center relative">
+      <div className="flex flex-col items-center justify-center h-[550px] bg-cyan-600 text-center relative">
         <img
           className="absolute z-0 w-full h-full object-cover object-center brightness-90"
           src="/images/landing-hero.jpeg"
           alt=""
         />
         <motion.div
-          className="z-10 p-14 bg-opacity-70 bg-slate-700 rounded-lg"
+          className="flex flex-col justify-center items-center gap-3 min-w-[300px] w-[80%] max-w-[1080px] min-h-[230px] z-10 p-14 bg-opacity-70 bg-slate-700 rounded-lg"
           variants={fadeIn}
         >
-          <h1 className="font-semibold z-20">{translation.hero}</h1>
+          <h1 className="font-semibold z-20 text-5xl md:text-[4rem] ">
+            {translation.hero}
+          </h1>
           <span>{translation.heroSubheading}</span>
         </motion.div>
       </div>
       <section>
-        <div className="w-[clamp(300px, 90%, 1200px)] bg-slate-400 bg-opacity-70 border rond"></div>
+        <div className="flex justify-around items-center w-[clamp(300px, 80%, 1200px)] bg-slate-400 bg-opacity-10 border rounded min-h-[400px] m-20 flex-col md:flex-row md:m-6">
+          <motion.div className={spotlightItem}>
+            <img
+              src="/images/landing/product.svg"
+              alt=""
+              className={spotlightPicSize}
+            />
+            {translation.spotlight[0]}
+          </motion.div>
+          <motion.div className={spotlightItem}>
+            <img
+              src="/images/landing/quality.svg"
+              alt=""
+              className={spotlightPicSize}
+            />
+            {translation.spotlight[1]}
+          </motion.div>
+          <motion.div className={spotlightItem}>
+            <img
+              src="/images/landing/growth.svg"
+              alt=""
+              className={spotlightPicSize}
+            />
+            {translation.spotlight[2]}
+          </motion.div>
+        </div>
       </section>
     </motion.div>
   );

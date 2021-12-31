960514347351f46bd0d166d31632a104cf69aaae
Tim <harvard90873@gmail.com>
2021-12-31 16:37:08 +0100

    show our vision(idk what i fing wrote)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { LocaleType } from "../components/LanguageToggle";
 import { enIndex, esIndex, zhIndex } from "../translations";
 import { getTranslation } from "../utils/getTranslation";
 import { AnimateVisible } from "../components/FramerViewportAnimate";
+import { useState } from "react";
 
 const start = {
   y: 100,
@@ -42,15 +43,89 @@ const Home: NextPage = () => {
   const router = useRouter();
   const locale = router.locale as LocaleType;
   const translation = getTranslation(locale, enIndex, esIndex, zhIndex);
+  const [showCover, setShowCover] = useState<boolean>(false);
 
   return (
     <motion.div exit={{ opacity: 0 }} initial="initial" animate="animate">
       {/* Hero */}
       <div className="flex flex-col items-center justify-center h-[550px] bg-cyan-600 text-center relative">
+        <div
+          className={`transition-all duration-700 ${
+            showCover ? "left-0" : "-left-[100vw]"
+          } w-full h-full bg-neutral-900 bg-opacity-80 absolute z-[11] p-9 flex flex-col items-start`}
+          onMouseEnter={() => {
+            setShowCover(true);
+          }}
+          onMouseLeave={() => {
+            setShowCover(false);
+          }}
+        >
+          <motion.h3
+            variants={{
+              initial: {
+                x: "-50vw",
+                opacity: 0,
+              },
+              animate: {
+                x: 0,
+                opacity: 1,
+                transition: {
+                  duration: 0.5,
+                  ease: [0.6, -0.05, 0.01, 0.99],
+                },
+              },
+            }}
+          >
+            Our vision
+          </motion.h3>
+          {showCover ? (
+            <motion.ul
+              className="pl-4 flex flex-col items-start"
+              variants={{
+                animate: {
+                  transition: {
+                    staggerChildren: 0.2,
+                  },
+                },
+              }}
+            >
+              {translation.visions.map((each) => (
+                <motion.li
+                  className="m-3"
+                  key={each}
+                  variants={{
+                    initial: {
+                      x: "-50vw",
+                      opacity: 0,
+                    },
+                    animate: {
+                      x: 0,
+                      opacity: 1,
+                      transition: {
+                        duration: 1,
+                        ease: [0.6, -0.05, 0.01, 0.99],
+                      },
+                    },
+                  }}
+                >
+                  {each}
+                </motion.li>
+              ))}
+            </motion.ul>
+          ) : (
+            ""
+          )}
+        </div>
         <img
           className="absolute z-0 w-full h-full object-cover object-center brightness-90"
           src="/images/landing-hero.jpeg"
           alt=""
+          onMouseEnter={() => {
+            setShowCover(true);
+          }}
+          onMouseLeave={() => {
+            setShowCover(false);
+          }}
         />
         <motion.div
           className="flex flex-col justify-center items-center gap-3 min-w-[300px] w-[80%] max-w-[1080px] min-h-[230px] z-10 p-14 bg-opacity-70 bg-slate-700 rounded-lg"
--- src/translations/index.ts
+++ src/translations/index.ts
@@ -8,15 +8,11 @@ export const enIndex = {
     "We provide you with high-quality products from reliable suppliers.",
     "We are seeking to build a global reputation and offer our products to more customers.",
   ],
-};
-
-export const esIndex: IndexTranslationType = {
-  hero: "Blue Ocean Comercio Internacional",
-  heroSubheading: "Un Negocio de Comercio Internacional",
-  spotlight: [
-    "Amplia gama de productos: desde artículos comerciales hasta herramientas convenientes, se los traemos desde cualquier parte del mundo.",
-    "Le proporcionamos productos de alta calidad de proveedores confiables.",
-    "Intentamos construir una reputación global y ofrecer nuestros productos a más clientes.",
+  visionHeading: "Our Vision",
+  visions: [
+    "Globalization - Increase the interconnectedness among people in the world by selling our products to customers all around the world.",
+    "Customer satisfaction - You are the reason why we exists, and our products are there for you.",
+    "Brand recognition - We are aiming to become a first-rate international trading business in Spain with our determination for growth and exceptional products.",
   ],
 };
 
@@ -27,5 +23,27 @@ export const zhIndex: IndexTranslationType = {
     "广泛的销售范围 - 从选购商品到便利工具，我们从世界各地带到你手里。",
     "我们给你提供从可靠的提供商进购高质量产品。",
     "我们正在努力建立全球声誉，并将我们的产品提供给更多客户。",
+  ],
+  visionHeading: "公司愿景",
+  visions: [
+    "全球化 - 以向全世界的客户销售产品来增加贸易往来和人们之间的联系。",
+    "消费者满意度 - 您是我们存在的原因，我们的产品的目标是给您提供便利。",
+    "品牌认知度 - 争做西班牙高端外贸国际型公司为目标；立足品牌、打造精品是公司的根本宗旨。",
+  ],
+};
+
+export const esIndex: IndexTranslationType = {
+  hero: "Blue Ocean Comercio Internacional",
+  heroSubheading: "Un Negocio de Comercio Internacional",
+  spotlight: [
+    "Amplia gama de productos: desde artículos comerciales hasta herramientas convenientes, se los traemos desde cualquier parte del mundo.",
+    "Le proporcionamos productos de alta calidad de proveedores confiables.",
+    "Intentamos construir una reputación global y ofrecer nuestros productos a más clientes.",
+  ],
+  visionHeading: "Nuestra Visión",
+  visions: [
+    "Globalización: aumentar la interconexión entre las personas en el mundo vendiendo nuestros productos a clientes de todo el mundo.",
+    "Satisfacción del cliente: usted es la razón por la que existimos y nuestros productos están ahí para usted.",
+    "Reconocimiento de marca: nuestro objetivo es convertirnos en una empresa comercial internacional con nuestra determinación de crecimiento y productos excepcionales.",
   ],
 };

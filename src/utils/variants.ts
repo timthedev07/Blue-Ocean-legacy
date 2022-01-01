61f41b01bc73ef97d446550e236e05c52294982f
Tim <harvard90873@gmail.com>
2022-01-01 14:43:20 +0100

    gather variants

---
+++ src/utils/variants.ts
@@ -0,0 +1,24 @@
+export const listItemVariants = {
+  initial: {
+    x: "-50vw",
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: [0.6, -0.05, 0.01, 0.99],
+    },
+  },
+};
+
+export const getStaggerVariants = (diff: number = 0.15) => {
+  return {
+    visible: {
+      transition: {
+        staggerChildren: diff,
+      },
+    },
+  };
+};

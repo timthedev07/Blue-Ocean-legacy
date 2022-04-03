b646395ae5d27962f42de6048cc0930efa074fe3
Tim <harvard90873@gmail.com>
2022-04-03 18:43:41 +0200

    complete basic mobile nav

--- src/components/navigation/mobile/MobileNavElement.tsx
+++ src/components/navigation/mobile/MobileNavElement.tsx
@@ -2,19 +2,26 @@ import Link from "next/link";
 import { FC } from "react";
 
 interface SidebarElementProps {
-  icon?: any;
   name: string;
   href: string;
+  onClick?: Function;
 }
 
 export const MobileNavElement: FC<SidebarElementProps> = ({
   name,
   href,
-  icon,
+  onClick,
 }) => {
   return (
     <Link href={href} passHref>
-      <div>{name}</div>
+      <div
+        onClick={() => {
+          if (onClick) onClick();
+        }}
+        className="cursor-pointer w-auto px-4 py-3 capitalize transition-all duration-150 bg-neutral-200 bg-opacity-0 hover:bg-opacity-30"
+      >
+        {name}
+      </div>
     </Link>
   );
 };
--- src/components/navigation/mobile/MobileNavList.tsx
+++ src/components/navigation/mobile/MobileNavList.tsx
@@ -1,4 +1,10 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { FC } from "react";
+import { enNav, esNav, zhNav } from "../../../translations/components/Nav";
+import { getTranslation } from "../../../utils/getTranslation";
+import { NAV_LINKS } from "../Nav";
+import { MobileNavElement } from "./MobileNavElement";
 import { NavDisclosure } from "./MobileNavTrigger";
 
 interface MobileNavListProps {
@@ -8,14 +14,37 @@ interface MobileNavListProps {
 export const MobileNavList: FC<MobileNavListProps> = ({
   disclosure: [open, setOpen],
 }) => {
-  open;
+  const { locale } = useRouter();
+  const t = getTranslation(locale, enNav, esNav, zhNav);
+
   return (
     <>
       <div
-        className={`z-[9998] transition-all duration-500 h-screen absolute top-0 bottom-0 w-60 bg-slate-900 filter brightness-[1.2] ${
+        className={`pt-20 z-[9998] transition-all duration-500 h-screen absolute top-0 bottom-0 w-60 bg-slate-900 filter brightness-[1.2] ${
           !open ? "-left-[100%]" : "left-0"
         }`}
-      ></div>
+      >
+        <Link href="/" passHref>
+          <img
+            src="/nav-logo.svg"
+            alt=""
+            onClick={() => {
+              setOpen(false);
+            }}
+            className="h-14 mx-4 mb-6 w-auto cursor-pointer"
+          />
+        </Link>
+        {NAV_LINKS.map((each) => (
+          <MobileNavElement
+            key={each.name}
+            name={t[each.name]}
+            href={each.href}
+            onClick={() => {
+              setOpen(false);
+            }}
+          />
+        ))}
+      </div>
 
       {/* overlay */}
       <div

9b965139d8c117ef6c4353c3508f88e44def3d33
Tim <harvard90873@gmail.com>
2022-04-04 21:08:37 +0200

    initial crappy about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { enAbout, esAbout, zhAbout } from "../translations/about";
+import { getTranslation } from "../utils/getTranslation";
 
 const About: NextPage = () => {
-  return <>About us</>;
+  const { locale } = useRouter();
+  const t = getTranslation(locale, enAbout, esAbout, zhAbout);
+  return <div className="p-12">{t.info}</div>;
 };
 
 export default About;

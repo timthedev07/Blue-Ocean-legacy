ea26ed79ae58e3ec0e81af363a840e39135143a3
Tim <harvard90873@gmail.com>
2022-04-04 19:26:16 +0200

    some translations

--- src/translations/about.ts
+++ src/translations/about.ts
@@ -3,3 +3,11 @@ export type AboutTranslationType = typeof zhAbout;
 export const zhAbout = {
   info: "Blue Ocean International Trade，S.L.成立于 2020 年 8 月，公司由旅居西班牙的华人 ZHEN BAO 先生创立于西班牙风景秀丽的地中海沿岸城市 ALICANTE。公司是一家从事进出口贸易创新型企业，提供全球采购贸易服务，争做西班牙高端外贸国际型公司为目标；立足品牌、打造精品是公司的根本宗旨；拥有“BLUE OCEAN”注册品牌商标；公司于 2022 年 1 月正式进军亚马逊商城欧洲站，主营自有品牌的精修五金工具、太阳眼镜及小家电类产品，进一步丰富了公司的产品阵容；每一位用户都是我们真诚的朋友，追求每一位用户的满意是我们的终极目标！",
 };
+
+export const enAbout: AboutTranslationType = {
+  info: "Blue Ocean International Trade, S.L. was founded in August 2020 by the Chinese entrepreneur currently living in Spain. We are an international firm primarily focused on worldwide commercial activity aiming to become a first-rate global trading business. Our business moved in to Amazon in January 2022, and our typical products include convenient tools, well-designed sunglasses, and other types of home appliances; this further enriches the starting point of our journey. Every customer is our most valuable friend and we hope that our service and products can bring you real convenience and satisfaction in life.",
+};
+
+export const esAbout: AboutTranslationType = {
+  info: "Blue Ocean International Trade, S.L. fue fundada en agosto de 2020 por el empresario chino que actualmente vive en España. Somos una firma internacional enfocada principalmente en la actividad comercial mundial con el objetivo de convertirnos en un negocio comercial global de primer nivel. Nuestro negocio se mudó a Amazon en enero de 2022 y nuestros productos típicos incluyen herramientas convenientes, anteojos de sol bien diseñados y otros tipos de electrodomésticos; esto enriquece aún más el punto de partida de nuestro viaje. Cada cliente es nuestro amigo más valioso y esperamos que nuestro servicio y productos puedan brindarle una verdadera comodidad y satisfacción en la vida.",
+};

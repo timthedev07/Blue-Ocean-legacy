6d35f9d3142b6a65daffca3ea6da76c6e7a5de20
Tim <harvard90873@gmail.com>
2022-04-12 21:36:19 +0200

    building product entry component

---
+++ src/components/Product.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { chopWordForUI } from "../utils/chopTextForUI";
+
+interface ProductProps {
+  name: string;
+  price: number;
+  photoUrl: string;
+}
+
+export const Product: FC<ProductProps> = ({ name, price, photoUrl }) => {
+  return (
+    <div className="w-60 border border-neutral-300 border-opacity-10 rounded-md bg-slate-900/30 cursor-pointer transition-all duration-150 hover:bg-slate-800/40 hover:shadow-2xl transform hover:-translate-y-[1.5px]">
+      <img src={photoUrl} className="w-auto rounded-t-md" alt={name} />
+      <div className="px-4 py-2 pt-3">
+        <span className="text-xl">{chopWordForUI(name, 6)}</span>
+        <div className="flex justify-start gap-5 mt-3">
+          <span className="text-green-400 text-base">€{price}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/utils/chopTextForUI.ts
@@ -0,0 +1,18 @@
+/**
+ * Cuts off the rest of the text after the `i`th character (0 indexed)
+ * @param text
+ * @param i
+ */
+export const chopTextForUI = (text: string, i: number = 18) => {
+  return text.slice(0, i) + "...";
+};
+
+/**
+ * Cuts off the rest of the text after the `i`th word (0 indexed)
+ * @param text
+ * @param i
+ */
+export const chopWordForUI = (text: string, i: number = 5) => {
+  const words = text.split(" ");
+  return words.slice(0, i).join(" ") + "...";
+};

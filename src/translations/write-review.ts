4bc3267a8683e90d266dd7dde3f8e1db4ebca6bd
Tim <harvard90873@gmail.com>
2022-04-18 01:26:22 +0200

    translations for new page

---
+++ src/translations/write-review.ts
@@ -0,0 +1,22 @@
+export type WriteReviewTranslationType = typeof zhWriteReview;
+
+export const zhWriteReview = {
+  authorName: "笔名",
+  rating: "评分",
+  review: "留言",
+  submit: "提交",
+};
+
+export const enWriteReview: WriteReviewTranslationType = {
+  authorName: "Write as",
+  rating: "Rating",
+  review: "Review",
+  submit: "Submit",
+};
+
+export const esWriteReview: WriteReviewTranslationType = {
+  authorName: "Escribe por",
+  rating: "Calificación",
+  review: "Crítica",
+  submit: "Subir",
+};

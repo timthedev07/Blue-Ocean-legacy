907f40fbac12e00aca9177e58f8464ad968e7809
Tim <harvard90873@gmail.com>
2022-04-17 19:04:56 +0200

    fix prop type

--- src/components/icons/social/Twitter.tsx
+++ src/components/icons/social/Twitter.tsx
@@ -1,8 +1,6 @@
-import { FC } from "react";
+import { FC, SVGProps } from "react";
 
-interface TwitterProps {}
-
-export const Twitter: FC<TwitterProps> = (props) => {
+export const Twitter: FC<SVGProps<SVGSVGElement>> = (props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"

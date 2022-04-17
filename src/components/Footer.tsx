afa931e471b597e107bd6f39f3df79cd59d1182f
Tim <harvard90873@gmail.com>
2022-04-17 15:44:11 +0200

    continue building footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,70 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { FC } from "react";
+import {
+  enFooter,
+  esFooter,
+  zhFooter,
+} from "../translations/components/footer";
+import { getTranslation } from "../utils/getTranslation";
 import { FooterLogo } from "./FooterLogo";
+import { Fb } from "./icons/social/Fb";
+import { Twitter } from "./icons/social/Twitter";
 
-interface FooterProps {}
+interface FooterLinkProps {
+  href: string;
+  name: string;
+}
 
-export const Footer: FC<FooterProps> = ({}) => {
+const FooterLink: FC<FooterLinkProps> = ({ href, name }) => {
   return (
-    <div className="sticky w-full h-72 bg-slate-900 p-6">
-      <FooterLogo className="w-72" />
+    <Link href={href} passHref>
+      <li className="capitalize cursor-pointer transition-all duration-200 hover:text-cyan-300">
+        {name}
+      </li>
+    </Link>
+  );
+};
+
+export const Footer: FC = ({}) => {
+  const { locale } = useRouter();
+  const t = getTranslation(locale, enFooter, esFooter, zhFooter);
+
+  return (
+    <div className="sticky w-full h-80 bg-slate-900 p-6">
+      <div className="flex justify-start gap-16">
+        <FooterLogo className="w-72" />
+        <article className="flex flex-col">
+          <h5 className="uppercase font-bold text-lg">{t.subheading0}</h5>
+          <ul className="text-sm">
+            <FooterLink href="/" name="Home" />
+            <FooterLink href="/about" name="About Us" />
+            <FooterLink href="/products" name="Products" />
+          </ul>
+        </article>
+        <article className="flex flex-col">
+          <h5 className="uppercase font-bold text-lg">{t.subheading1}</h5>
+          <ul className="text-sm">
+            <FooterLink href="/contact" name="Contact Us" />
+            <FooterLink href="/privacy-policy" name="Privacy Policy" />
+            <FooterLink
+              href="/terms-and-conditions"
+              name="Terms and Conditions"
+            />
+          </ul>
+        </article>
+        <article className="flex-1 flex justify-center items-center">
+          <Fb />
+          <Twitter />
+        </article>
+      </div>
+      <hr className="w-[90%] m-auto bg-slate-500" />
+      <div className="w-[90%] h-8 m-auto">
+        <span className="text-xs float-right my-5">
+          {new Date(Date.now()).getFullYear()} © Tim &nbsp;&nbsp;&nbsp; All
+          Rights Reserved
+        </span>
+      </div>
     </div>
   );
 };

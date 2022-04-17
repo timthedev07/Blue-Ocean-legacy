84769a196f2b46d45e2454f5950fcb03c327a4eb
Tim <harvard90873@gmail.com>
2022-04-17 15:43:51 +0200

    footer translations

---
+++ src/translations/components/footer.ts
@@ -0,0 +1,16 @@
+export type FooterTranslationType = typeof enFooter;
+
+export const enFooter = {
+  subheading0: "navigation",
+  subheading1: "support",
+};
+
+export const esFooter: FooterTranslationType = {
+  subheading0: "navegación",
+  subheading1: "ayuda",
+};
+
+export const zhFooter: FooterTranslationType = {
+  subheading0: "导航",
+  subheading1: "帮助",
+};

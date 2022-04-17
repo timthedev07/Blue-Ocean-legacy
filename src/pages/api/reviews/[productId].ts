0fd6d4a0762cf3989e8b5b12e16254a2865b9a89
Tim <harvard90873@gmail.com>
2022-04-17 21:02:21 +0200

    complete route

--- src/pages/api/reviews/[productId].ts
+++ src/pages/api/reviews/[productId].ts
@@ -2,7 +2,11 @@ import { NextApiHandler } from "next";
 import { collections, connectDB } from "../../../mongodb";
 import { Review } from "../../../mongodb/models/review";
 
-const handler: NextApiHandler = async (_, res) => {
+const handler: NextApiHandler = async (req, res) => {
+  if (req.method?.toLowerCase() !== "get") return;
+
+  const productId = req.query.productId as string;
+
   await connectDB();
 
   if (!collections.productReviews) {
@@ -10,8 +14,9 @@ const handler: NextApiHandler = async (_, res) => {
     return;
   }
 
+  const query = { productId };
   const reviews = (await collections.productReviews
-    .find()
+    .find(query)
     .toArray()) as any as Review[];
   res.json({
     reviews,

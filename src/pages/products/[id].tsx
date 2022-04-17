20868ece28198a23f3b4d650d5c594ad987cd35f
Tim <harvard90873@gmail.com>
2022-04-17 19:06:45 +0200

    building product info page

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,19 +1,37 @@
 import { NextPage, GetStaticProps } from "next";
 import { useRouter } from "next/router";
+import { LazyImage } from "../../components/LazyImage";
 import { productsData } from "../../products";
-
-const Spacer = () => {
-  return <div></div>;
-};
+import { getSingleTranslationObj } from "../../utils/getTranslation";
 
 const ProductDetails: NextPage<{ product: typeof productsData[0] }> = ({
   product,
 }) => {
   const { locale } = useRouter();
   locale;
+
+  const spacer = <div className="h-full flex-1 md:flex-grow flex-grow-0"></div>;
+
+  const test = "border rounded-lg border-red-400";
+
   return (
-    <div className="flex justify-center items-start">
-      <div></div>
+    <div className="flex justify-center items-start p-3 gap-3">
+      {spacer}
+      <div className=" flex-1 md:flex-grow-[3.5] lg:flex-grow-[2.5] flex justify-center gap-4 items-start border border-white rounded-lg">
+        <div className={`flex-1 flex-grow ${test} `}>
+          <LazyImage src="https://assets.vercel.com/image/upload/f_auto,c_limit,q_auto,w_96/front/home/new/leo.png" />
+        </div>
+        <div
+          className={`flex flex-col flex-1 flex-grow justify-start items-start p-6 gap-3 ${test}`}
+        >
+          <h4 className="font-bold">
+            {getSingleTranslationObj(locale, product.name)}
+          </h4>
+          <span className="text-emerald-400">{product.price}€</span>
+          <div>{getSingleTranslationObj(locale, product.description)}</div>
+        </div>
+      </div>
+      {spacer}
     </div>
   );
 };

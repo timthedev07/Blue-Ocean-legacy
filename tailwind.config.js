c43ec0b39cea13ee0472ee7e1d66177b40f203b1
Tim <harvard90873@gmail.com>
2022-04-17 15:43:57 +0200

    add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,36 @@ module.exports = {
       cursor: ["focus"],
     },
   },
+  theme: {
+    extend: {
+      keyframes: {
+        shine: {
+          "10%": {
+            opacity: "1",
+            top: "-30%",
+            left: "-30%",
+            "transition-property": "left, top, opacity",
+            "transition-duration": "0.7s, 0.7s, 0.15s",
+            "transition-timing-function": "ease",
+          },
+          "100%": {
+            opacity: "0",
+            top: "-30%",
+            left: "-30%",
+            "transition-property": "left, top, opacity",
+          },
+        },
+      },
+    },
+  },
   plugins: [require("@tailwindcss/typography")],
 };
+
+/**
+@keyframes shine{
+
+}
+Share
+Edit
+
+ */

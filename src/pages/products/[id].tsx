bed00949b94eba904f4bad686b9349c480134ad9
Tim <harvard90873@gmail.com>
2022-04-17 16:33:20 +0200

    update ssg

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,26 +1,42 @@
 import { NextPage, GetStaticProps } from "next";
 import { useRouter } from "next/router";
+import { productsData } from "../../products";
 
-const ProductDetails: NextPage<{ product: string }> = ({ product }) => {
-  const { query } = useRouter();
-  const productId = query.id as string;
-  return <>{productId}</>;
+const Spacer = () => {
+  return <div></div>;
+};
+
+const ProductDetails: NextPage<{ product: typeof productsData[0] }> = ({
+  product,
+}) => {
+  const { locale } = useRouter();
+  locale;
+  return (
+    <div className="flex justify-center items-start">
+      <div></div>
+    </div>
+  );
 };
 
 export async function getStaticPaths() {
   return {
-    paths: [{ params: { id: "0" } }],
+    paths: productsData.map((each) => ({
+      params: {
+        id: each.id,
+      },
+    })),
     fallback: true,
   };
 }
 
-export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params!;
-  console.log(id);
 
   return {
     props: {
-      product: "",
+      product: productsData.find((v) => {
+        return v.id == id;
+      }),
     },
   };
 };

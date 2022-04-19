dcc42103b37c4242fd1cc18bc511358dc92e2a09
Tim <harvard90873@gmail.com>
2022-04-19 11:13:15 +0200

    page heads

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -16,6 +16,7 @@ import { getSingleTranslation, getTranslation } from "../utils/getTranslation";
 import { enContact, esContact, zhContact } from "../translations/contact";
 import { toast } from "react-hot-toast";
 import { toastConfig } from "../utils/toastConfig";
+import { getHeadForPage } from "../utils/getHead";
 
 export interface ContactFormValues {
   name: string;
@@ -79,129 +80,137 @@ const Contact: NextPage = () => {
   const translation = getTranslation(locale, enContact, esContact, zhContact);
 
   return (
-    <motion.div
-      className="flex w-full flex-col justify-center items-center gap-6 p-3 md:p-16"
-      exit={{ opacity: 0 }}
-      initial="initial"
-      animate="animate"
-    >
-      <Heading>{translation.heading}</Heading>
-      <div className="rounded-lg border w-full h-60 p-5 bg-slate-900 flex justify-between">
-        <div className="grow shrink basis-1">
-          <h3 className="font-semibold">{translation.subheading}</h3>
-          <motion.ul className="pl-6" variants={getStaggerVariants(0.2)}>
-            <motion.li className="flex gap-3 m-3" variants={listItemVariants}>
-              <img className="w-6" src="/images/email.svg" alt="emailIcon" />
-              blueocean.co.official@gmail.com
-            </motion.li>
-            <motion.li className="flex gap-3 m-3" variants={listItemVariants}>
-              <img className="w-6" src="/images/phone.svg" alt="phoneIcon" />
-              643611778
-            </motion.li>
-          </motion.ul>
+    <>
+      {getHeadForPage({
+        description: "Contact Service - BOIT",
+        path: "/",
+        title: "Contact Service - BOIT",
+      })}
+
+      <motion.div
+        className="flex w-full flex-col justify-center items-center gap-6 p-3 md:p-16"
+        exit={{ opacity: 0 }}
+        initial="initial"
+        animate="animate"
+      >
+        <Heading>{translation.heading}</Heading>
+        <div className="rounded-lg border w-full h-60 p-5 bg-slate-900 flex justify-between">
+          <div className="grow shrink basis-1">
+            <h3 className="font-semibold">{translation.subheading}</h3>
+            <motion.ul className="pl-6" variants={getStaggerVariants(0.2)}>
+              <motion.li className="flex gap-3 m-3" variants={listItemVariants}>
+                <img className="w-6" src="/images/email.svg" alt="emailIcon" />
+                blueocean.co.official@gmail.com
+              </motion.li>
+              <motion.li className="flex gap-3 m-3" variants={listItemVariants}>
+                <img className="w-6" src="/images/phone.svg" alt="phoneIcon" />
+                643611778
+              </motion.li>
+            </motion.ul>
+          </div>
+          <div className="h-full md:flex justify-end hidden md:justify-center items-center grow shrink basis-1">
+            <motion.img
+              src="/images/pin.svg"
+              alt="pinIcon"
+              className="w-24 h-24 md:w-32 md:h-32"
+              variants={pinVariants}
+            />
+          </div>
         </div>
-        <div className="h-full md:flex justify-end hidden md:justify-center items-center grow shrink basis-1">
-          <motion.img
-            src="/images/pin.svg"
-            alt="pinIcon"
-            className="w-24 h-24 md:w-32 md:h-32"
-            variants={pinVariants}
-          />
-        </div>
-      </div>
-      <div className="w-full h-full flex gap-8 flex-col-reverse md:flex-row">
-        <div className="p-6 min-w-[20rem] w-full border rounded-lg grow shrink basis-0 h-full bg-slate-900">
-          <Box className="my-4 text-left">
-            <form
-              onSubmit={formik.handleSubmit}
-              className="flex flex-col gap-3"
-            >
-              <FormControl isRequired>
-                <FormLabel as="legend" htmlFor="contact-name">
-                  {translation.labels.name}
-                </FormLabel>
-                <Input
-                  id="contact-name"
-                  className="border-black"
-                  placeholder="John Doe"
-                  required
-                  value={formik.values.name}
-                  name="name"
-                  variant="flushed"
-                  onChange={formik.handleChange}
-                />
-              </FormControl>
-              <FormControl isRequired>
-                <FormLabel as="legend" htmlFor="contact-email">
-                  {translation.labels.email}
-                </FormLabel>
-                <Input
-                  id="contact-email"
-                  className="border-black"
-                  required
-                  placeholder="google@gmail.com"
-                  type="email"
-                  value={formik.values.email}
-                  variant="flushed"
-                  name="email"
-                  onChange={formik.handleChange}
-                />
-              </FormControl>
-              <FormControl>
-                <FormLabel as="legend" htmlFor="contact-phoneNumber">
-                  {translation.labels.phoneNumber}
-                </FormLabel>
-                <Input
-                  className="border-black"
-                  id="contact-phoneNumber"
-                  name="phoneNumber"
-                  variant="flushed"
-                  onChange={formik.handleChange}
-                  value={formik.values.phoneNumber}
-                />
-              </FormControl>
-              <FormControl isRequired>
-                <FormLabel as="legend" htmlFor="contact-message">
-                  {translation.labels.message}
-                </FormLabel>
-                <Textarea
-                  id="contact-message"
-                  className="border-black"
-                  name="message"
-                  onChange={formik.handleChange}
-                  variant="flushed"
-                  resize={"none"}
-                  value={formik.values.message}
-                />
-              </FormControl>
-              <Button type="submit">{translation.submit}</Button>
-            </form>
-          </Box>
-        </div>
-        <div className="grow shrink basis-0">
-          <motion.iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6217.914351759194!2d-0.4487569153254044!3d38.40311527582633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6239aea5f80955%3A0x501aba76bd580f0b!2sCarrer%20la%20Melva%2C%2003540%20Alacant!5e1!3m2!1sen!2ses!4v1641047121756!5m2!1sen!2ses"
-            width="100%"
-            height="100%"
-            className="border-none rounded-lg"
-            allowFullScreen
-            loading="lazy"
-            variants={{
-              initial: {
-                scale: 0.01,
-              },
-              animate: {
-                scale: 1,
-                transition: {
-                  duration: 0.5,
-                  ease: [0.6, -0.05, 0.01, 0.99],
+        <div className="w-full h-full flex gap-8 flex-col-reverse md:flex-row">
+          <div className="p-6 min-w-[20rem] w-full border rounded-lg grow shrink basis-0 h-full bg-slate-900">
+            <Box className="my-4 text-left">
+              <form
+                onSubmit={formik.handleSubmit}
+                className="flex flex-col gap-3"
+              >
+                <FormControl isRequired>
+                  <FormLabel as="legend" htmlFor="contact-name">
+                    {translation.labels.name}
+                  </FormLabel>
+                  <Input
+                    id="contact-name"
+                    className="border-black"
+                    placeholder="John Doe"
+                    required
+                    value={formik.values.name}
+                    name="name"
+                    variant="flushed"
+                    onChange={formik.handleChange}
+                  />
+                </FormControl>
+                <FormControl isRequired>
+                  <FormLabel as="legend" htmlFor="contact-email">
+                    {translation.labels.email}
+                  </FormLabel>
+                  <Input
+                    id="contact-email"
+                    className="border-black"
+                    required
+                    placeholder="google@gmail.com"
+                    type="email"
+                    value={formik.values.email}
+                    variant="flushed"
+                    name="email"
+                    onChange={formik.handleChange}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel as="legend" htmlFor="contact-phoneNumber">
+                    {translation.labels.phoneNumber}
+                  </FormLabel>
+                  <Input
+                    className="border-black"
+                    id="contact-phoneNumber"
+                    name="phoneNumber"
+                    variant="flushed"
+                    onChange={formik.handleChange}
+                    value={formik.values.phoneNumber}
+                  />
+                </FormControl>
+                <FormControl isRequired>
+                  <FormLabel as="legend" htmlFor="contact-message">
+                    {translation.labels.message}
+                  </FormLabel>
+                  <Textarea
+                    id="contact-message"
+                    className="border-black"
+                    name="message"
+                    onChange={formik.handleChange}
+                    variant="flushed"
+                    resize={"none"}
+                    value={formik.values.message}
+                  />
+                </FormControl>
+                <Button type="submit">{translation.submit}</Button>
+              </form>
+            </Box>
+          </div>
+          <div className="grow shrink basis-0">
+            <motion.iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6217.914351759194!2d-0.4487569153254044!3d38.40311527582633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6239aea5f80955%3A0x501aba76bd580f0b!2sCarrer%20la%20Melva%2C%2003540%20Alacant!5e1!3m2!1sen!2ses!4v1641047121756!5m2!1sen!2ses"
+              width="100%"
+              height="100%"
+              className="border-none rounded-lg"
+              allowFullScreen
+              loading="lazy"
+              variants={{
+                initial: {
+                  scale: 0.01,
                 },
-              },
-            }}
-          ></motion.iframe>
+                animate: {
+                  scale: 1,
+                  transition: {
+                    duration: 0.5,
+                    ease: [0.6, -0.05, 0.01, 0.99],
+                  },
+                },
+              }}
+            ></motion.iframe>
+          </div>
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import { AnimateVisible } from "../components/FramerViewportAnimate";
 import { useState } from "react";
 import Link from "next/link";
 import { getStaggerVariants, listItemVariants } from "../utils/variants";
+import { getHeadForPage } from "../utils/getHead";
 
 const start = {
   y: 100,
@@ -41,117 +42,125 @@ const Home: NextPage = () => {
   const [showCover, setShowCover] = useState<boolean>(false);
 
   return (
-    <motion.div exit={{ opacity: 0 }} initial="initial" animate="animate">
-      {/* Hero */}
-      <div className="with-scroll-indicator flex flex-col items-center justify-start gap-4 md:gap-14 h-[550px] bg-cyan-700 text-center relative">
-        <div
-          className={`transition-transform duration-1000 transform-gpu ${
-            showCover ? "translate-x-0" : "-translate-x-[1000vw]"
-          } w-full h-full bg-neutral-900 bg-opacity-80 absolute p-9 flex flex-col items-start z-40`}
-          onMouseEnter={() => {
-            setShowCover(true);
-          }}
-          onMouseLeave={() => {
-            setShowCover(false);
-          }}
-        >
-          <motion.h3
-            variants={{
-              initial: {
-                x: "-50vw",
-                opacity: 0,
-              },
-              animate: {
-                x: 0,
-                opacity: 1,
-                transition: {
-                  duration: 0.5,
-                  ease: [0.6, -0.05, 0.01, 0.99],
-                },
-              },
+    <>
+      {getHeadForPage({
+        description:
+          "The Homepage of Blue Ocean - An International Trading Business",
+        path: "/",
+        title: "BOIT - Homepage",
+      })}
+      <motion.div exit={{ opacity: 0 }} initial="initial" animate="animate">
+        {/* Hero */}
+        <div className="with-scroll-indicator flex flex-col items-center justify-start gap-4 md:gap-14 h-[550px] bg-cyan-700 text-center relative">
+          <div
+            className={`transition-transform duration-1000 transform-gpu ${
+              showCover ? "translate-x-0" : "-translate-x-[1000vw]"
+            } w-full h-full bg-neutral-900 bg-opacity-80 absolute p-9 flex flex-col items-start z-40`}
+            onMouseEnter={() => {
+              setShowCover(true);
+            }}
+            onMouseLeave={() => {
+              setShowCover(false);
             }}
           >
-            {translation.visionHeading}
-          </motion.h3>
-          {showCover ? (
-            <div className="flex flex-col justify-between h-full w-full">
-              <motion.ul
-                className="pl-4 flex flex-col items-start"
-                variants={getStaggerVariants(0.2)}
-              >
-                {translation.visions.map((each) => (
-                  <motion.li
-                    className="m-4 text-left"
-                    key={each}
-                    variants={listItemVariants}
-                  >
-                    {each}
-                  </motion.li>
-                ))}
-              </motion.ul>
-              <div className="w-full">
-                <Link href="/about" passHref>
-                  <button className=" p-3 px-6 bg-sky-600 rounded-md uppercase transition duration-200 hover:bg-sky-600">
-                    Learn more
-                  </button>
-                </Link>
+            <motion.h3
+              variants={{
+                initial: {
+                  x: "-50vw",
+                  opacity: 0,
+                },
+                animate: {
+                  x: 0,
+                  opacity: 1,
+                  transition: {
+                    duration: 0.5,
+                    ease: [0.6, -0.05, 0.01, 0.99],
+                  },
+                },
+              }}
+            >
+              {translation.visionHeading}
+            </motion.h3>
+            {showCover ? (
+              <div className="flex flex-col justify-between h-full w-full">
+                <motion.ul
+                  className="pl-4 flex flex-col items-start"
+                  variants={getStaggerVariants(0.2)}
+                >
+                  {translation.visions.map((each) => (
+                    <motion.li
+                      className="m-4 text-left"
+                      key={each}
+                      variants={listItemVariants}
+                    >
+                      {each}
+                    </motion.li>
+                  ))}
+                </motion.ul>
+                <div className="w-full">
+                  <Link href="/about" passHref>
+                    <button className=" p-3 px-6 bg-sky-600 rounded-md uppercase transition duration-200 hover:bg-sky-600">
+                      Learn more
+                    </button>
+                  </Link>
+                </div>
               </div>
-            </div>
-          ) : (
-            ""
-          )}
-        </div>
-        <div
-          className="absolute z-0 w-full h-full object-cover object-center brightness-90 bg-cyan-600"
-          onMouseEnter={() => {
-            setShowCover(true);
-          }}
-          onMouseLeave={() => {
-            setShowCover(false);
-          }}
-        />
-        {/* <div className="w-full flex justify-end z-[41]">
+            ) : (
+              ""
+            )}
+          </div>
+          <div
+            className="absolute z-0 w-full h-full object-cover object-center brightness-90 bg-cyan-600"
+            onMouseEnter={() => {
+              setShowCover(true);
+            }}
+            onMouseLeave={() => {
+              setShowCover(false);
+            }}
+          />
+          {/* <div className="w-full flex justify-end z-[41]">
           <img src="/light-logo.png" alt="" className="w-60" />
         </div> */}
-        <motion.div
-          className="flex flex-col justify-center items-center gap-3 h-min w-[80%] max-w-[1080px] z-10 bg-opacity-70 text-zinc-200 rounded-lg mt-36 lg:mt-48"
-          variants={fadeInHero}
-        >
-          <h1 className="font-semibold z-20 text-5xl md:text-[4rem]">
-            {translation.hero}
-          </h1>
-          <span>{translation.heroSubheading}</span>
-        </motion.div>
-      </div>
-      <section className="p-5 md:p-8">
-        <AnimateVisible className="flex justify-around items-center w-[clamp(300px, 80%, 1200px)] bg-slate-400 bg-opacity-10 border rounded min-h-[400px] flex-col md:flex-row">
-          <AnimateVisible variants={fadeIn} className={spotlightItem}>
-            <img
-              src="/images/landing/product.svg"
-              alt=""
-              className={spotlightPicSize}
-            />
-            {translation.spotlight[0]}
+          <motion.div
+            className="flex flex-col justify-center items-center gap-3 h-min w-[80%] max-w-[1080px] z-10 bg-opacity-70 text-zinc-200 rounded-lg mt-36 lg:mt-48"
+            variants={fadeInHero}
+          >
+            <h1 className="font-semibold z-20 text-5xl md:text-[4rem]">
+              {translation.hero}
+            </h1>
+            <span>{translation.heroSubheading}</span>
+          </motion.div>
+        </div>
+        <section className="p-5 md:p-8">
+          <AnimateVisible className="flex justify-around items-center w-[clamp(300px, 80%, 1200px)] bg-slate-400 bg-opacity-10 border rounded min-h-[400px] flex-col md:flex-row">
+            <AnimateVisible variants={fadeIn} className={spotlightItem}>
+              <img
+                src="/images/landing/product.svg"
+                alt=""
+                className={spotlightPicSize}
+              />
+              {translation.spotlight[0]}
+            </AnimateVisible>
+            <AnimateVisible variants={fadeIn} className={spotlightItem}>
+              <img
+                src="/images/landing/quality.svg"
+                alt=""
+                className={spotlightPicSize}
+              />
+              {translation.spotlight[1]}
+            </AnimateVisible>
+            <AnimateVisible variants={fadeIn} className={spotlightItem}>
+              <img
+                src="/images/landing/growth.svg"
+                alt=""
+                className={spotlightPicSize}
+              />
+              {translation.spotlight[2]}
+            </AnimateVisible>
           </AnimateVisible>
-          <AnimateVisible variants={fadeIn} className={spotlightItem}>
-            <img
-              src="/images/landing/quality.svg"
-              alt=""
-              className={spotlightPicSize}
-            />
-            {translation.spotlight[1]}
-          </AnimateVisible>
-          <AnimateVisible variants={fadeIn} className={spotlightItem}>
-            <img
-              src="/images/landing/growth.svg"
-              alt=""
-              className={spotlightPicSize}
-            />
-            {translation.spotlight[2]}
-          </AnimateVisible>
-        </AnimateVisible>
-      </section>
-    </motion.div>
+        </section>
+      </motion.div>
+    </>
   );
 };
 

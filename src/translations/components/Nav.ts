b6bf407ffc0ee0b2a989992e761406d3db0d25e2
Tim <harvard90873@gmail.com>
2022-05-21 10:04:57 +0200

    fix translation

--- src/translations/components/Nav.ts
+++ src/translations/components/Nav.ts
@@ -4,16 +4,19 @@ export const enNav = {
   aboutUs: "About Us",
   products: "Products",
   contactUs: "Contact Us",
+  home: "Home",
 };
 
 export const esNav: NavTranslationType = {
   aboutUs: "Nosotros",
   contactUs: "Contactarnos",
   products: "Productos",
+  home: "Pagína Principal",
 };
 
 export const zhNav: NavTranslationType = {
   aboutUs: "关于我们",
   contactUs: "联系我们",
   products: "产品",
+  home: "主页",
 };

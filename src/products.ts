feb57c07c3daa9a798bc023d31e3325410a55d31
Tim <harvard90873@gmail.com>
2022-07-01 00:22:18 +0200

    mtfk fix

--- src/products.ts
+++ src/products.ts
@@ -11,6 +11,6 @@ export const productsData = [
       es: "Destornillador profesional y preciso, 145 escenarios todo en uno; diseñado para dispositivos móviles, portátiles, PC, relojes, gafas, etc.",
       zh: "Blue Ocean专业精密螺丝刀，145合一磁性修复螺丝刀套装；适用于手机、平板电脑、PC、笔记本电脑、手表、眼镜、iPhone、iPad等",
     },
-    price: 30,
+    price: 32.99,
   },
 ];

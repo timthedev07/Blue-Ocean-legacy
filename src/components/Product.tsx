1b3cf266527e7a05da09ce031f6d8285b5e54f4e
Tim <harvard90873@gmail.com>
2022-06-30 22:11:39 +0200

    fix component

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { FC } from "react";
 import { chopWordForUI } from "../utils/chopTextForUI";
 import { cloudinaryInstance } from "../cloudinary";
-import { LazyImage } from "./LazyImage";
+import Image from "next/image";
 
 interface ProductProps {
   name: string;
@@ -17,13 +17,9 @@ export const Product: FC<ProductProps> = ({ name, price, id }) => {
   return (
     <Link href={`/products/${id}`} passHref>
       <div className="w-60 border border-neutral-300 border-opacity-10 rounded-md bg-slate-900/30 cursor-pointer transition-all duration-150 hover:bg-slate-800/40 hover:shadow-2xl transform hover:-translate-y-[1.5px]">
-        <LazyImage
-          src={thumbnailURL}
-          className="rounded-t-md"
-          placeholderHeight="rounded-t-md"
-          containerStyles="w-full h-auto"
-          alt={name}
-        />
+        <div className="w-full h-64 relative rounded-t-md overflow-hidden">
+          <Image layout="fill" src={thumbnailURL} alt={name} />
+        </div>
         <div className="px-4 py-2 pt-3">
           <span className="text-xl">{chopWordForUI(name, 6)}</span>
           <div className="flex justify-start gap-5 mt-3">

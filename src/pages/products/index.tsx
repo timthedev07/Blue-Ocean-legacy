75972e7e4e9061a60b1eb84154e1568120701980
Tim <harvard90873@gmail.com>
2022-07-01 00:19:22 +0200

    god damn it

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps, NextPage } from "next";
+import { NextPage } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { Product } from "../../components/Product";
